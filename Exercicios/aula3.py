5df59a40ec8e4d6927636f066ce08145422ee0c4
kito-vini <marcusviva11@gmail.com.br>
2021-04-17 14:49:32 -0300

    exercicios da aula3

---
+++ Exercicios/aula3.py
@@ -0,0 +1,6 @@
+combustivelGasto = int(input("Digite a quantidade gasta de combustível: "))
+kilometrosPercorrido = int(input("Digite a quantidade de KM percorridos: "))
+
+custoMedio = kilometrosPercorrido / combustivelGasto
+
+print("O custo médio de combustível é de {}Kmpl".format(custoMedio))
---
+++ variaveis.py
@@ -0,0 +1,8 @@
+# criando variável usando um input
+nome = input("Digite seu nome: ")
+
+# criando variável usando input e concatenando com outra variável
+idade = input("Olá {}, Digite sua idade: ".format(nome))
+
+# imprimindo as váriaveis dos inputs
+print("O aluno {} tem {} anos".format(nome, idade))

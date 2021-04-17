7e20e58665e29a0f38cb8db683e66ba6cc0e0af4
kito-vini <marcusviva11@gmail.com.br>
2021-04-17 15:13:40 -0300

    Melhoria do código

--- Exercicios/aula3.py
+++ Exercicios/aula3.py
@@ -3,4 +3,4 @@ kilometrosPercorrido = int(input("Digite a quantidade de KM percorridos: "))
 
 custoMedio = kilometrosPercorrido / combustivelGasto
 
-print("O custo médio de combustível é de {}Kmpl".format(custoMedio))
+print("O custo médio de combustível é de {}Km/l".format(custoMedio))

46d5d046c8ffc4e9ade584a05839002cf17e122c
kito-vini <marcusviva11@gmail.com.br>
2021-04-20 19:49:02 -0300

    criação da primeira int-graf

--- Exercicios/aula4nv2.py
+++ Exercicios/aula4nv2.py
@@ -10,8 +10,8 @@ media = (av1 + av2 + av3) / 3
 
 if media >= 7.0:
     print('Sua média é: {}. Parabéns {}! Você está aprovado!'.format(media, nome))
-else:
-    if media <= 6.0:
+else: 
+if media <= 6.0:
         print('Sua média é: {}. Que pena {}, Você foi reprovado!'.format(media, nome))
     else:
         print('Sua média é: {}. Atenção {}! Você está de recuperação! Estude mais.'.format(media, nome))
